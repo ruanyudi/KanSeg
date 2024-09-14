2905b4b243205172216a202f5622b5a9c91cdfa1
ruanyudi <977953751@qq.com>
2024-09-14 18:40:54 +0800

    ADD visual_metrics.py

---
+++ visual_metrics.py
@@ -0,0 +1,36 @@
+import argparse
+import json
+import matplotlib.pyplot as plt
+
+METRICS=['total_loss','segm/AP','bbox/AP']
+
+def get_parser():
+    parser = argparse.ArgumentParser(description="maskdino demo for builtin configs")
+    parser.add_argument(
+        "--metrics-file",
+        default="./output/metrics.json",
+        metavar="FILE",
+        help="path to metric file",
+    )
+    return parser
+
+def show_metric():
+    for METRIC in METRICS:
+        metric2show = []
+        for metric in AP_metrics:
+            metric2show.append(metric[METRIC])
+        plt.plot(metric2show,label=METRIC)
+    plt.legend()
+    plt.savefig('visual_metric.png',dpi=300)
+
+if __name__ == '__main__':
+    args = get_parser().parse_args()
+    AP_metrics = []
+    for line in open(args.metrics_file,'r'):
+        metrics = json.loads(line)
+        if "segm/AP" in metrics.keys():
+            AP_metrics.append(metrics)
+    print(AP_metrics[0].keys())
+    show_metric()
+
+    
